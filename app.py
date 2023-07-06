6c14375429e8386cff5a73012b0d33361a834ece
AbhisheKumar1616 <63004757+AbhisheKumar1616@users.noreply.github.com>
2023-07-06 15:09:06 +0000

    Adder functionality to login page and checked with some dummy data

--- app.py
+++ app.py
@@ -38,10 +38,18 @@ def register():
         return redirect(url_for('home'))
     return render_template("register.html",title="Registeration",form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form=Login()
-    return render_template("login.html",title="Login",form=form)
+    form = Login()
+    if form.validate_on_submit():
+        if form.user_id.data == 'Abhi1616' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__=="__main__":
